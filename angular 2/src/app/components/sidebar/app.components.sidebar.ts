e0846605dc14f69b35f40757bde5b299d6225902
Dyam Marcano <dyam.marcano@gmail.com>
2016-07-15 16:41:24 -0400

    fixes

--- angular 2/src/app/components/sidebar/app.components.sidebar.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'sidebar',
-  directives: [],
-  templateUrl: 'app/sidebar/sidebar.tpl.html',
-  styleUrls: ['app/sidebar/sidebar.css']
-})
-export class Sidebar {}
--- angular 2/src/app/login/app.login.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'root-document',
-  directives: [],
-  templateUrl: 'app/login/login.tpl.html',
-  styleUrls: ['app/login/login.css']
-})
-export class Login {}
